11cf6283ca53cc486ffd0d14bd48db1fabe22bab
Grzegorz Łukasik <71289836+lukasikgrzegorz@users.noreply.github.com>
2025-08-12 20:48:30 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://lukasikgrzegorz.github.io/aidlazielonych',
-  base: '',
+  site: 'https://lukasikgrzegorz.github.io',
+  base: '/aidlazielonych',
   vite: {
     plugins: [tailwindcss()]
   }
